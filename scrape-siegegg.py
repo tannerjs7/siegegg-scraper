6efd40ad01a73c2db7ce6765914a867863042561
Tanner Schilperoort <tannerschilperoort@gmail.com>
2024-06-23 16:01:30 -0400

    Make scraper create one file rather than many
    Before, the SiegeGG scraper would create one txt
    file for each competition. Now, it combines all the
    player data into one file. The "use" file currently
    prints the 10 players with the most kills. Also
    the file names were changed.

--- scrape-siegegg.py
+++ scrape-siegegg.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-# import csv
 
 
 #Convert data from 'X-Y (Z)' format to [X, Y]
@@ -16,20 +15,23 @@ def get_sibling(sib, n):
   return sib 
 
 
-for num in range(1, 2):
+players = {}
+for num in range(1, 500):
+
   #Scrape data from SiegeGG
   link = 'https://siege.gg/competitions/' + str(num)
   page_to_scrape = requests.get(link)
   soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
-
   table = soup.find('table', attrs={'class':'w-100'})
+
+  #If there are no stats on the SiegeGG page, skip the page
+  if table == None: continue
   tr = table.tbody.tr
   
-  #Create a dictionary with entries of the following format:
-  #'PLAYER': [KILLS, DEATHS, ENTRY_KILLS, ENTRY_DEATHS, MAPS, CLUTCHES, PLANTS]
-  players = {}
+  #Add or update the dictionary with player entries of the following format:
+  #'player': [kills, deaths, entry_kills, entry_deaths, maps, clutches, plants]
   while tr != ']':
-    player = tr.td.span.a['href'][9:]
+    player = tr.td.span.a['href'][9:].split('-')[1]
     data = get_sibling(tr.td, 2)
     kd = get_kd(data.text)
 
@@ -45,9 +47,22 @@ for num in range(1, 2):
     data = get_sibling(data, 1)
     plants = [int(data.text)]
 
-    players[player] = kd + entry_kd + maps + clutches + plants
+    if player not in players:
+      players[player] = kd + entry_kd + maps + clutches + plants
+    else:
+      players[player][0] += kd[0]
+      players[player][1] += kd[1]
+      players[player][2] += entry_kd[0]
+      players[player][3] += entry_kd[1]
+      players[player][4] += maps[0]
+      players[player][5] += clutches[0]
+      players[player][6] += plants[0]
     tr = tr.next_sibling
-  print(players)
+  print(num)
 
   # with open('./competitions/' + str(num) + '.txt', 'w') as file:
   #   file.write(str(players))
+
+#Write the dictionary to the 'competitions/all.txt' file as a string
+with open('./competitions/' + 'all' + '.txt', 'w') as file:
+  file.write(str(players))
--- use-siegegg.py
+++ use-siegegg.py
@@ -1,10 +1,13 @@
 import ast
 
-file_path = '499.txt'
+file_path = './competitions/all.txt'
 
 with open(file_path, 'r') as file:
 	file_content = file.read()
 
 players = ast.literal_eval(file_content)
-for player in players:
-	print(player, players[player])
+
+sorted_by_kills = sorted(players.items(), key=lambda item: item[1][0], reverse=True)
+for counter, player in enumerate(sorted_by_kills):
+	if counter == 20: break
+	print(player[0] + ' kills: ' + str(player[1][0]))
