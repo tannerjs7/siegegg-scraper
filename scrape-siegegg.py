2ac14258e286dc00d527a28a0afe906f33cf792d
Tanner Schilperoort <tannerschilperoort@gmail.com>
2024-07-08 19:31:28 -0400

    Add boilerplate

--- scrape-siegegg.py
+++ scrape-siegegg.py
@@ -2,67 +2,75 @@ from bs4 import BeautifulSoup
 import requests
 
 
-#Convert data from 'X-Y (Z)' format to [X, Y]
-def get_kd(data):
-  data = data.split('-')
-  return [int(data[0]), int(data[1].split()[0])]
+def main():
+
+  #Convert data from 'X-Y (Z)' format to [X, Y]
+  def get_kd(data):
+    data = data.split('-')
+    return [int(data[0]), int(data[1].split()[0])]
 
 
-#Get the nth sibling
-def get_sibling(sib, n):
-  for _ in range(n):
-    sib = sib.next_sibling
-  return sib 
+  #Get the nth sibling
+  def get_sibling(sib, n):
+    for _ in range(n):
+      sib = sib.next_sibling
+    return sib 
 
 
-players = {}
-for num in range(1, 500):
+  players = {}
+  for num in range(1, 500):
 
-  #Scrape data from SiegeGG
-  link = 'https://siege.gg/competitions/' + str(num)
-  page_to_scrape = requests.get(link)
-  soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
-  table = soup.find('table', attrs={'class':'w-100'})
+    #Scrape data from SiegeGG
+    link = 'https://siege.gg/competitions/' + str(num)
+    page_to_scrape = requests.get(link)
+    soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
+    table = soup.find('table', attrs={'class':'w-100'})
 
-  #If there are no stats on the SiegeGG page, skip the page
-  if table == None: continue
-  tr = table.tbody.tr
-  
-  #Add or update the dictionary with player entries of the following format:
-  #'player': [kills, deaths, entry_kills, entry_deaths, maps, clutches, plants]
-  while tr != ']':
-    player = tr.td.span.a['href'][9:].split('-')[1]
-    data = get_sibling(tr.td, 2)
-    kd = get_kd(data.text)
+    #If there are no stats on the SiegeGG page, skip the page
+    if table == None: continue
+    tr = table.tbody.tr
+    
+    #Add or update the dictionary with player entries of the following format:
+    #'player': [kills, deaths, entry_kills, entry_deaths, maps, clutches, plants]
+    while tr != ']':
+      player = tr.td.span.a['href'][9:].split('-')[1]
+      data = get_sibling(tr.td, 2)
+      kd = get_kd(data.text)
 
-    data = get_sibling(data, 1)
-    entry_kd = get_kd(data.text)
+      data = get_sibling(data, 1)
+      entry_kd = get_kd(data.text)
 
-    data = get_sibling(data, 1)
-    maps = [int(data.text)]
+      data = get_sibling(data, 1)
+      maps = [int(data.text)]
 
-    data = get_sibling(data, 4)
-    clutches = [int(data.text)]
+      data = get_sibling(data, 4)
+      clutches = [int(data.text)]
 
-    data = get_sibling(data, 1)
-    plants = [int(data.text)]
+      data = get_sibling(data, 1)
+      plants = [int(data.text)]
 
-    if player not in players:
-      players[player] = kd + entry_kd + maps + clutches + plants
-    else:
-      players[player][0] += kd[0]
-      players[player][1] += kd[1]
-      players[player][2] += entry_kd[0]
-      players[player][3] += entry_kd[1]
-      players[player][4] += maps[0]
-      players[player][5] += clutches[0]
-      players[player][6] += plants[0]
-    tr = tr.next_sibling
-  print(num)
+      if player not in players:
+        players[player] = kd + entry_kd + maps + clutches + plants
+      else:
+        players[player][0] += kd[0]
+        players[player][1] += kd[1]
+        players[player][2] += entry_kd[0]
+        players[player][3] += entry_kd[1]
+        players[player][4] += maps[0]
+        players[player][5] += clutches[0]
+        players[player][6] += plants[0]
+      tr = tr.next_sibling
 
-  # with open('./competitions/' + str(num) + '.txt', 'w') as file:
-  #   file.write(str(players))
+    #Print the current number to track progress
+    print(num)
 
-#Write the dictionary to the 'competitions/all.txt' file as a string
-with open('./competitions/' + 'all' + '.txt', 'w') as file:
-  file.write(str(players))
+    # with open('./competitions/' + str(num) + '.txt', 'w') as file:
+    #   file.write(str(players))
+
+  #Write the dictionary to the 'competitions/all.txt' file as a string
+  with open('./competitions/' + 'all' + '.txt', 'w') as file:
+    file.write(str(players))
+
+
+if __name__ == '__main__':
+  main()
--- use-stats.py
+++ use-stats.py
@@ -1,44 +1,50 @@
 import ast
 
-file_path = './competitions/all.txt'
 
-with open(file_path, 'r') as file:
-	file_content = file.read()
+def main():
+	file_path = './competitions/all.txt'
 
-players = ast.literal_eval(file_content)
+	with open(file_path, 'r') as file:
+		file_content = file.read()
 
-def get_stat(stat_name, stat_index, number_of_players):
-	print('Top ' + str(number_of_players) + ' players by ' + stat_name)
-	for counter, player in enumerate(
-		sorted(players.items(), key=lambda item: item[1][stat_index], reverse=True)
-	):
-		if counter == number_of_players: break
-		print(player[0] + ' ' + stat_name + ': ' + str(player[1][stat_index]))
+	players = ast.literal_eval(file_content)
 
-def get_kills(number_of_players=10):
-	get_stat('kills', 0, number_of_players)
+	def get_stat(stat_name, stat_index, number_of_players):
+		print('Top ' + str(number_of_players) + ' players by ' + stat_name)
+		for counter, player in enumerate(
+			sorted(players.items(), key=lambda item: item[1][stat_index], reverse=True)
+		):
+			if counter == number_of_players: break
+			print(player[0] + ' ' + stat_name + ': ' + str(player[1][stat_index]))
 
-def get_deaths(number_of_players=10):
-	get_stat('deaths', 1, number_of_players)
+	def get_kills(number_of_players=10):
+		get_stat('kills', 0, number_of_players)
 
-def get_entry_kills(number_of_players=10):
-	get_stat('entry kills', 2, number_of_players)
+	def get_deaths(number_of_players=10):
+		get_stat('deaths', 1, number_of_players)
 
-def get_entry_deaths(number_of_players=10):
-	get_stat('entry deaths', 3, number_of_players)
+	def get_entry_kills(number_of_players=10):
+		get_stat('entry kills', 2, number_of_players)
 
-def get_maps(number_of_players=10):
-	get_stat('maps', 4, number_of_players)
+	def get_entry_deaths(number_of_players=10):
+		get_stat('entry deaths', 3, number_of_players)
 
-def get_clutches(number_of_players=10):
-	get_stat('clutches', 5, number_of_players)
+	def get_maps(number_of_players=10):
+		get_stat('maps', 4, number_of_players)
 
-def get_plants(number_of_players=10):
-	get_stat('plants', 6, number_of_players)
+	def get_clutches(number_of_players=10):
+		get_stat('clutches', 5, number_of_players)
 
-def get_players(first_letters=''):
-	for player in sorted(players.items(), key=lambda item: item[0]):
-		if player[0].startswith(first_letters):
-			print(player[0])
+	def get_plants(number_of_players=10):
+		get_stat('plants', 6, number_of_players)
 
-get_players()
+	def get_players(first_letters=''):
+		for player in sorted(players.items(), key=lambda item: item[0]):
+			if player[0].startswith(first_letters):
+				print(player[0])
+
+	get_players()
+
+
+if __name__ == '__main__':
+	main()
